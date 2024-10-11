09a70f9f6258b61674ca08cd7f1a08e711780da5
Zhou <zwy9990.0@gmail.com>
2024-10-11 20:46:48 +1000

    表单验证

--- react-jike/src/pages/Login/index.js
+++ react-jike/src/pages/Login/index.js
@@ -1,5 +1,43 @@
-const Login = () =>{
-    return <div>this is Login</div>
+import { Card, Form, Input, Button, Checkbox } from 'antd'
+import './index.scss'
+
+const Login = () => {
+    return (
+        <div className="login">
+            <Card className="login-container">
+                {/* 登录表单 */}
+                <Form validateTrigger="onBlur">
+                    <Form.Item name="mobile"
+                        rules={[
+                            {
+                                required: true,
+                                message: '手机号不能为空',
+                            },
+                            {
+                                pattern: /^1[3-9]\d{9}$/,
+                                message: '手机号格式不正确',
+                            }
+                        ]}>
+                        <Input size="large" placeholder="请输入手机号" />
+                    </Form.Item>
+                    <Form.Item name="code"
+                        rules={[
+                            {
+                                required: true,
+                                message: '验证码不能为空',
+                            },
+                        ]}>
+                        <Input size="large" placeholder="请输入验证码" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button style={{ color: 'red', backgroundColor: 'pink' }} type="primary" htmlType="submit" size="large" block>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
 }
 
 export default Login
