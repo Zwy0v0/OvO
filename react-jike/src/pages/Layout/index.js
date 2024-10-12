d896b4974484b149032b2bb139ace335696b520d
Zhou <zwy9990.0@gmail.com>
2024-10-12 14:06:34 +1000

    token注入header

--- react-jike/src/pages/Layout/index.js
+++ react-jike/src/pages/Layout/index.js
@@ -1,4 +1,11 @@
+import { request } from "@/utils"
+import { useEffect } from "react"
+
 const Layout = () =>{
+    //测试token注入请求头
+    useEffect(() => {
+        request.get('/user/profile')
+    },[])
     return <div>this is layout</div>
 }
 
