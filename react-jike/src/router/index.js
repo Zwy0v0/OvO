f4f6db09d26584f105e4fded9edf64dc5ad93760
Zhou <zwy9990.0@gmail.com>
2024-10-12 15:15:21 +1000

    二级路由配置

--- react-jike/src/router/index.js
+++ react-jike/src/router/index.js
@@ -1,12 +1,33 @@
-import {createBrowserRouter} from 'react-router-dom'
+import {createBrowserRouter,Navigate} from 'react-router-dom'
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
+import Article from '@/pages/Article'
+import Home from '@/pages/Home'
+import Publish from '@/pages/Publish'
 import { AuthRoute } from '@/components/AuthRoute'
 
 const router = createBrowserRouter([
     {
         path:'/',
-        element: <AuthRoute><Layout/></AuthRoute>
+        element: <AuthRoute><Layout/></AuthRoute>,
+        children:[
+            {
+                path:'/',
+                element:<Navigate to = {'home'}/>
+            },
+            {
+                path:'home',
+                element:<Home/>
+            },
+            {
+                path:'article',
+                element:<Article/>
+            },
+            {
+                path:'publish',
+                element:<Publish/>
+            }
+        ]
     },
     {
         path:'/login',
